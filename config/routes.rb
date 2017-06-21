0c3c70475db6c9981474550a183f2c06acb9beac
Gustavo Moreira <gustavomn93@gmail.com>
2017-06-20 23:50:48 -0300

    Adding create users method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resources :users
 
+  get 'users/new'
+  get '/signup' => 'users#new'
+  
   get 'twitter/index'
 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
