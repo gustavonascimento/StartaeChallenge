ae21f1322f8a9682a8d3bc5f2973fa6be66aa362
Gustavo Moreira <gustavomn93@gmail.com>
2017-06-21 23:38:05 -0300

    Creating some method to CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      log_in @user
+      redirect_to '/'
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to '/'
+    else
+      render 'edit'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'bruno/index'
 
   get 'contaifer/index'
@@ -29,10 +31,16 @@ Rails.application.routes.draw do
   get '/welcome' => 'welcome#index'
 
   get 'users/new'
+
   get '/signup' => 'users#new'
 
+  get 'edit' => 'users#edit'
 
   get 'twitter/index'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
